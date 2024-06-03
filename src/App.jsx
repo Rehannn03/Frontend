cf61c8cafa78682a9ba8edd75939eaa27d6c742e
REHAN SHAH <spamhomeacc123@gmail.com>
2024-06-03 22:24:10 +0530

    chore: Remove unused code and update react-router-dom import in App.jsx and main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import './App.css'
 import Products from './Pages/Products'
-import { Outlet } from 'react-router-dom'
+
 import Cart from './Pages/Cart.jsx'
-import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements,Routes} from 'react-router-dom'
+import { Route,Routes} from 'react-router-dom'
 function App() {
 
   return (
